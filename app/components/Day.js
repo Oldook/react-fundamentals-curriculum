a7d1330ffdd66cdbff50c63062aad0cc00b0e392
Ivan Kalugin <ivan.kalugin@noveogroup.com>
2017-08-18 19:08:11 +0700

    almost daay 7

---
+++ app/components/Day.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+class Day extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+}
+
+Day.propTypes = {
+    
+}
+
+module.exports = Day;
--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,12 +1,41 @@
 var React = require('react');
+var Loading = require('./Loading');
+var queryString = require('query-string');
+var api = require('../utils/api');
 
 class Forecast extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            forecast: [],
+            loading: true
+        }
+    } 
+
+    componentDidMount() {
+        this.setState(function () {
+            return {
+                loading: true
+            }
+        })
+        api.getForecast(queryString.parse(this.props.location.search).city).then(function (response) {
+            this.setState(function () {
+                console.log(response);
+                return {
+                    loading: false,
+                    forecast: response
+                }
+            })
+        }.bind(this));
+    }
+
     render () {
-        return (
-            <div className="app-navbar">
-                <h1>Forecast</h1>
-            </div>
-        )
+        return this.state.loading ?
+            <Loading />
+            :
+            <p>city</p>
+            
     }
 }
 
