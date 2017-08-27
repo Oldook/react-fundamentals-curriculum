91f843f98946f3818f891bd626d02174afe94c0e
Oldook <ivan.kalugin@noveogroup.com>
2017-08-27 22:45:21 +0700

    step 10

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 
-module.exports = {
+var config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -52,3 +52,16 @@ module.exports = {
         })
     ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    )
+}
+
+module.exports = config;
